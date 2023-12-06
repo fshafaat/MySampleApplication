50fb4a2cc7d29f80158017847448d758d7e7f0af
Fawaz Shafaat <fawaz.shafaat@digital.justice.gov.uk>
2023-12-06 01:09:58 +0000

    More tests and requirement for at least one underscore

--- MySampleApplication.UnitTests/PasswordValidationHelperTests.cs
+++ MySampleApplication.UnitTests/PasswordValidationHelperTests.cs
@@ -11,7 +11,7 @@ namespace MySampleApplication.UnitTests
         [InlineData("12", false)]
         [InlineData("1234", false)]
         [InlineData("12345678", false)]
-        [InlineData("Password!", true)]
+        [InlineData("Password1234567_", true)]
         public void Password_Must_Be_More_Than_8_Characters(string password, bool expected)
         {
             bool result = PasswordValidationHelper.IsPasswordValid(password);
@@ -19,11 +19,10 @@ namespace MySampleApplication.UnitTests
         }
 
         [Theory]
-        [InlineData("password", false)]
+        [InlineData("passwordislong", false)]
         [InlineData("123456789", false)]
-        [InlineData("PassWord12", true)]
-        [InlineData("Registration12!", true)]
-        [InlineData("Password!", true)]
+        [InlineData("_PassWord12", true)]
+        [InlineData("Reg__istration12!", true)]
         public void Password_Must_Has_At_Least_One_Capital_Letter(string password, bool expected)
         {
             bool result = PasswordValidationHelper.IsPasswordValid(password);
@@ -33,9 +32,9 @@ namespace MySampleApplication.UnitTests
         [Theory]
         [InlineData("PASSWORD12!", false)]
         [InlineData("123456789", false)]
-        [InlineData("PassWord12", true)]
-        [InlineData("Registration12!", true)]
-        [InlineData("Password!", true)]
+        [InlineData("PassW_ord12", true)]
+        [InlineData("Reg_istration12!", true)]
+        [InlineData("Pass_word!", false)]
         public void Password_Must_Has_At_Least_One_Lowercase_Letter(string password, bool expected)
         {
             bool result = PasswordValidationHelper.IsPasswordValid(password);
@@ -46,9 +45,21 @@ namespace MySampleApplication.UnitTests
         [InlineData("PASSWORDAbc", false)]
         [InlineData("123456789", false)]
         [InlineData("Password!", false)]
-        [InlineData("PassWord12", true)]
-        [InlineData("Registration12!", true)]
+        [InlineData("PassWord12_", true)]
+        [InlineData("Registration12!_", true)]
         public void Password_Must_Has_At_Least_One_Number(string password, bool expected)
+        {
+            bool result = PasswordValidationHelper.IsPasswordValid(password);
+            result.Should().Be(expected);
+        }
+
+        [Theory]
+        [InlineData("PASSWORDAbc", false)]
+        [InlineData("123456789", false)]
+        [InlineData("Password!", false)]
+        [InlineData("PassWord12_", true)]
+        [InlineData("Registration12!__", true)]
+        public void Password_Must_Has_An_Underscore(string password, bool expected)
         {
             bool result = PasswordValidationHelper.IsPasswordValid(password);
             result.Should().Be(expected);
--- MySampleApplication/Helpers/PasswordValidationHelper.cs
+++ MySampleApplication/Helpers/PasswordValidationHelper.cs
@@ -10,11 +10,13 @@ namespace MySampleApplication.Helpers
             var hasACapitalLetter = new Regex(@"[A-Z]+");
             var hasALowerCaseLetter = new Regex(@"[a-z]+");
             var hasANumber = new Regex(@"[0-9]+");
+            var hasAnUnderscore = new Regex(@"[_]");
 
             if (hasMoreThan8Characters.IsMatch(password)
                 && hasACapitalLetter.IsMatch(password)
                 && hasALowerCaseLetter.IsMatch(password)
-                && hasANumber.IsMatch(password))
+                && hasANumber.IsMatch(password)
+                && hasAnUnderscore.IsMatch(password))
                 return true;
 
             return false;
